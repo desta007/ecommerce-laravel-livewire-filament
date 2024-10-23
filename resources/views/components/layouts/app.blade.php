a1b7e45a9cc424db0571a282df3b50c2de66dfab
Desta CP <detajuventini@gmail.com>
2024-10-23 11:03:00 +0700

    feat: add to cart

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -15,6 +15,9 @@
     {{ $slot }}
     @livewire('partials.footer')
     @livewireScripts
+
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <x-livewire-alert::scripts />
 </body>
 
 </html>
